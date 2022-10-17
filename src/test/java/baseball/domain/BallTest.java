f1803331d4cbc2b91b535236266930561aa3b323
heesang <dlagmltkd12@naver.com>
2022-10-18 00:59:06 +0900

    test: Ball의 숫자가 같은 숫자인지 확인

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -1,17 +1,30 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BallTest {
+    private Ball ball1;
+    private Ball ball2;
+
+    @BeforeEach
+    void setUp() {
+        ball1 = new Ball(1);
+        ball2 = new Ball(1);
+    }
 
     @DisplayName("Ball이 값 객체인지 검증")
     @Test
     void create() {
-        Ball ball1 = new Ball(1);
-        Ball ball2 = new Ball(1);
+        assertThat(ball1).isEqualTo(ball2);
+    }
+
+    @DisplayName("Ball의 숫자가 같은 숫자인지 확인")
+    @Test
+    void isSame() {
         assertThat(ball1).isEqualTo(ball2);
     }
 }
