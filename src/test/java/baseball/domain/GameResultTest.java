c60c1fd703bb3b1d32ee23c36d36a553d319d39f
heesang <dlagmltkd12@naver.com>
2022-10-18 02:48:47 +0900

    test: GameResult 테스트 구현

---
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultTest {
+
+    private GameResult gameResult;
+
+    @BeforeEach
+    void setUp() {
+        gameResult = new GameResult();
+    }
+
+    @DisplayName("strike 증가")
+    @Test
+    void increaseStrike() {
+        gameResult.increaseStrike();
+
+        GameResultResponseDto responseDto = gameResult.toResponseDto();
+        assertThat(responseDto.getStrike()).isEqualTo(1);
+        assertThat(responseDto.getBall()).isEqualTo(0);
+    }
+
+    @DisplayName("ball 증가")
+    @Test
+    void increaseBall() {
+        gameResult.increaseBall();
+
+        GameResultResponseDto responseDto = gameResult.toResponseDto();
+        assertThat(responseDto.getStrike()).isEqualTo(0);
+        assertThat(responseDto.getBall()).isEqualTo(1);
+    }
+}
