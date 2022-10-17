d013d2fcf5d7a3acf4726bdb54fcc00e9683e3c3
heesang <dlagmltkd12@naver.com>
2022-10-18 01:34:18 +0900

    test: 두개의 볼이 스트라이크인지 테스트

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -9,22 +9,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BallTest {
     private Ball ball1;
     private Ball ball2;
+    private Ball ball3;
+    private Ball ball4;
 
     @BeforeEach
     void setUp() {
-        ball1 = new Ball(1);
-        ball2 = new Ball(1);
+        ball1 = new Ball(1, 1);
+        ball2 = new Ball(1, 1);
+        ball3 = new Ball(2, 1);
+        ball4 = new Ball(3, 3);
     }
 
-    @DisplayName("Ball이 값 객체인지 검증")
+    @DisplayName("볼이 스트라이크 인지 검증")
     @Test
-    void create() {
-        assertThat(ball1).isEqualTo(ball2);
-    }
-
-    @DisplayName("Ball의 숫자가 같은 숫자인지 확인")
-    @Test
-    void isSame() {
-        assertThat(ball1).isEqualTo(ball2);
+    void isStrike() {
+        assertThat(ball1.isStrike(ball2)).isTrue();
     }
 }
