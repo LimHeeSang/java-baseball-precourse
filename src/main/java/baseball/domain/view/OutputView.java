1ae7a00d92e7d5e350f85f63a1f2565d586d76d4
heesang <dlagmltkd12@naver.com>
2022-10-18 05:14:18 +0900

    fix: OutputView 형식 수정

--- src/main/java/baseball/domain/view/OutputView.java
+++ src/main/java/baseball/domain/view/OutputView.java
@@ -1,4 +1,4 @@
-package baseball.domain.Controller;
+package baseball.domain.view;
 
 import baseball.domain.dto.GameResultResponseDto;
 
@@ -19,13 +19,13 @@ public class OutputView {
 
     private static void printStrike(GameResultResponseDto responseDto) {
         if (responseDto.getStrike() != 0 && responseDto.getBall() == 0) {
-            System.out.printf("%d + 스트라이크\n", responseDto.getStrike());
+            System.out.printf("%d스트라이크\n", responseDto.getStrike());
         }
     }
 
     private static void printBall(GameResultResponseDto responseDto) {
         if (responseDto.getStrike() == 0 && responseDto.getBall() != 0) {
-            System.out.printf("%d + 볼\n", responseDto.getBall());
+            System.out.printf("%d볼\n", responseDto.getBall());
         }
     }
 
