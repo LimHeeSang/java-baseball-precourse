16f7a25da396e545685016f650eb2c32cdb7487e
heesang <dlagmltkd12@naver.com>
2022-10-18 03:40:53 +0900

    feat: 컴퓨터 3개의 랜덤볼 선택 Computer 클래스 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,10 +1,14 @@
 package baseball.domain;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
-public class Computer extends Player{
+public class Computer extends Player {
 
-    public Computer(List<Ball> balls) {
-        super(balls);
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    public static final int COUNT = 3;
+
+    public Computer() {
+        super(Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT));
     }
 }
