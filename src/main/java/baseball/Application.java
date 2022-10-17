2a27943a10c8aff67435b200a64581e5ef14d894
heesang <dlagmltkd12@naver.com>
2022-10-18 08:26:09 +0900

    feat: 어플리케이션 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.domain.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Controller controller = new Controller();
+        controller.run();
     }
 }
