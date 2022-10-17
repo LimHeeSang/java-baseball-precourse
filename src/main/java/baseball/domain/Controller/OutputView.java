2edccaf95e4152ccb9896a1055791b836e89a2e4
heesang <dlagmltkd12@naver.com>
2022-10-18 04:17:23 +0900

    feat: OutputView

---
+++ src/main/java/baseball/domain/Controller/OutputView.java
@@ -0,0 +1,37 @@
+package baseball.domain.Controller;
+
+import baseball.domain.dto.GameResultResponseDto;
+
+public class OutputView {
+
+    public static void printResult(GameResultResponseDto responseDto) {
+        printNothing(responseDto);
+        printBall(responseDto);
+        printStrike(responseDto);
+        printBallAndStrike(responseDto);
+    }
+
+    private static void printBallAndStrike(GameResultResponseDto responseDto) {
+        if (responseDto.getStrike() != 0 && responseDto.getBall() != 0) {
+            System.out.printf("%d볼 %d스트라이크\n", responseDto.getBall(), responseDto.getStrike());
+        }
+    }
+
+    private static void printStrike(GameResultResponseDto responseDto) {
+        if (responseDto.getStrike() != 0 && responseDto.getBall() == 0) {
+            System.out.printf("%d + 스트라이크\n", responseDto.getStrike());
+        }
+    }
+
+    private static void printBall(GameResultResponseDto responseDto) {
+        if (responseDto.getStrike() == 0 && responseDto.getBall() != 0) {
+            System.out.printf("%d + 볼\n", responseDto.getBall());
+        }
+    }
+
+    private static void printNothing(GameResultResponseDto responseDto) {
+        if (responseDto.getStrike() == 0 && responseDto.getBall() == 0) {
+            System.out.println("낫싱");
+        }
+    }
+}
