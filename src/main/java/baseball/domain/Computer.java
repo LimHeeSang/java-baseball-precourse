71725a22f4cc7cf28d31a98044bba24d0b3c8f2c
heesang <dlagmltkd12@naver.com>
2022-10-18 00:53:34 +0900

    feat: 컴퓨터가 3개의 수를 가지는 기능

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+
+    private final List<Ball> balls;
+
+    public Computer(List<Ball> balls) {
+        this.balls = balls;
+    }
+}
