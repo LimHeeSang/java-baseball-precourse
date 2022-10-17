337bb962b49cf18b3407763302417e2a1ba8ae61
heesang <dlagmltkd12@naver.com>
2022-10-18 02:51:51 +0900

    test: GameResultResponseDtoTest 테스트 구현

---
+++ src/test/java/baseball/domain/GameResultResponseDtoTest.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameResultResponseDtoTest {
+
+    private GameResultResponseDto responseDto;
+
+    @BeforeEach
+    void setUp() {
+        responseDto = new GameResultResponseDto(1, 2);
+    }
+
+    @DisplayName("dto getStrike 검증")
+    @Test
+    void getStrike() {
+        Assertions.assertThat(responseDto.getStrike()).isEqualTo(1);
+    }
+
+    @DisplayName("dto getBall 검증")
+    @Test
+    void getBall() {
+        Assertions.assertThat(responseDto.getBall()).isEqualTo(2);
+    }
+}
