8814b6c58ccbc1a468afaf7f0c031465ccd461d6
heesang <dlagmltkd12@naver.com>
2022-10-18 05:16:16 +0900

    refactor: RadomNumber 클래스 로직 분리

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,14 +1,8 @@
 package baseball.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Computer extends Player {
 
-    public static final int START_INCLUSIVE = 1;
-    public static final int END_INCLUSIVE = 9;
-    public static final int COUNT = 3;
-
     public Computer() {
-        super(Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT));
+        super(RandomNumber.createRandomNumbers());
     }
 }
---
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+
+    private RandomNumber() {
+    }
+
+    public static List<Integer> createRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < Referee.GAME_ROUND) {
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        return numbers;
+    }
+}
