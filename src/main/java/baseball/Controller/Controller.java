a0d12da872d207cbc1c44374b88ee86eeaaadff9
heesang <dlagmltkd12@naver.com>
2022-10-18 19:06:39 +0900

    feat: Controller 기능

--- src/main/java/baseball/Controller/Controller.java
+++ src/main/java/baseball/Controller/Controller.java
@@ -9,20 +9,22 @@ import baseball.view.OutputView;
 
 public class Controller {
 
+    public static final int END_NUMBER = 2;
     private Referee referee;
 
     public void run() {
         Computer computer = new Computer();
-
-        boolean reGameFlag = true;
         referee = new Referee(new Player(InputView.inputNumber()), computer);
 
+        // TODO: 2022-10-18 리팩토링 필요
+        boolean reGameFlag = true;
         while (reGameFlag) {
             GameResultResponseDto resultResponseDto = referee.playGame();
             OutputView.printResult(resultResponseDto);
 
             if (resultResponseDto.isGameWin()) {
-                if (InputView.endGame() == 2) {
+                OutputView.printEndGame();
+                if (InputView.inputRestart() == END_NUMBER) {
                     break;
                 }
                 referee.changeComputer(new Computer());
